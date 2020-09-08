c410035a467e111e349fef15ef4cdea3f2d49182
malunan <723325701@qq.com>
2020-09-08 21:42:38 +0800

    域名

--- docs/.vuepress/public/https/api.js
+++ docs/.vuepress/public/https/api.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 // 172.21.0.17
 export const getblog=(theme)=>{
-    return axios.get('https://127.0.0.1:3000/getblog?theme='+theme);
+    return axios.get('http://zmln1021.cn:3000/getblog?theme='+theme);
 }
 export const addMsg=(theme)=>{
     return axios(
         {
             method:"post",
-            url:"https://127.0.0.1:3000/addblog",
+            url:"http://zmln1021.cn:3000/addblog",
             data:theme
         }
     )
